262882cd2f8eab5f4854571874f0b47a72db6a5b
farmanaditya <nadityaalya@gmail.com>
2024-09-16 17:01:10 +0700

    marker geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import { Geolocation } from '@capacitor/geolocation';
+import Graphic from '@arcgis/core/Graphic';
+import Point from '@arcgis/core/geometry/Point';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +13,47 @@ import { Geolocation } from '@capacitor/geolocation';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {}
   private latitude: number | any;
-  private longitude: number | any;
+  private longtitude: number| any;
+
   public async ngOnInit() {
     const position = await Geolocation.getCurrentPosition();
     this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
+    this.longtitude = position.coords.longitude;
+   const map = new Map({
+    basemap: "topo-vector"
+   });
 
-    const map = new Map({
-      basemap: 'topo-vector'
-    });
-    const view = new MapView({
-      container: 'container', // Reference to the DOM node that will contain the view
-      map: map, // Reference to the map object created before the view
-      center: [this.latitude, this.longitude], // Coordinates of the center of the view
-      zoom: 4 // Sets zoom level
-    });
-  }
+   const view = new MapView({
+    container: "container",
+    map: map,
+    zoom: 12,
+    center: [this.longtitude, this.latitude]
+   })
+
+
+  const markerSymbol = new SimpleMarkerSymbol({
+    color: "red",
+    outline: {
+      color: "black",
+      width: 2
+    }
+  });
+
+  const point = new Point({
+    longitude: this.longtitude,
+    latitude: this.latitude
+  });
+
+
+  const pointGraphic = new Graphic({
+    geometry: point,
+    symbol: markerSymbol
+  });
+
+
+  view.graphics.add(pointGraphic);
+}
 
 }
